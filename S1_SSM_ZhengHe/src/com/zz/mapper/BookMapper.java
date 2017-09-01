1118ea90f49db0e42c92b453e40f456ce8daf2a3
monkey0376 <27955144+monkey0376@users.noreply.github.com>
2017-09-01 14:52:12 +0800

    SSM整合注解形式

---
+++ S1_SSM_ZhengHe/src/com/zz/mapper/BookMapper.java
@@ -0,0 +1,20 @@
+package com.zz.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import com.zz.domain.Book;
+
+/**
+ * BookMapper接口
+ * */
+public interface BookMapper {
+
+	/**
+	 * 查询所有图书
+	 * @return 图书对象集合
+	 * */
+	@Select(" select * from tb_book ")
+	List<Book> findAll();
+	
+}
